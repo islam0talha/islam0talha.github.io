dcee460c4648498d1e08fd2e7e3f2eea5e2f52a9
Islam_Talha <islam_talha@hotmail.com>
2022-11-20 13:57:35 +0200

    Updates

--- sw.js
+++ sw.js
@@ -27,7 +27,7 @@ workbox.core.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-8b14208270b920f69268.js"
+    "url": "webpack-runtime-650ee4ff89c32372b96b.js"
   },
   {
     "url": "framework-0f8de3526885dd073c5e.js"
@@ -37,7 +37,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "8f7013f3307e65e4c01a4bbf40610602"
+    "revision": "0c3858fb456ce1af1560e82f676e7cdc"
   },
   {
     "url": "component---cache-caches-gatsby-plugin-offline-app-shell-js-f277fb9b28e1661bc3bb.js"
